eae2a904650d52eb737c6a458e71e41d0dff9b0c
mhaks <mike.hakanson@gmail.com>
2023-07-24 12:00:55 -0600

    added topproducts, product get

--- Controllers/ShoppingController.cs
+++ Controllers/ShoppingController.cs
@@ -22,26 +22,12 @@ namespace CommerceApiDemo.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<ProductCategory> Get()
+        [Route("ProductCategories")]
+        public async Task<ActionResult<IEnumerable<ProductCategory>>> GetProductCategories()
         {
             if (_context == null || _context.ProductCategory == null)
             {
                 return new List<ProductCategory>();
-            }
-
-            return _context.ProductCategory
-                .AsNoTracking()
-                .OrderBy(x => x.Title)
-                .ToList();
-        }
-
-        /*
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductCategory>>> Get()
-        {
-            if(_context == null || _context.ProductCategory == null) 
-            {
-                return NotFound();
             }
 
             return await _context.ProductCategory
@@ -49,6 +35,41 @@ namespace CommerceApiDemo.Controllers
                 .OrderBy(x => x.Title)
                 .ToListAsync();
         }
-        */
+
+
+        [HttpGet]
+        [Route("TopProducts")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetTopProducts()
+        {
+            return  await _context.Product
+                .OrderByDescending(a => _context.OrderProduct.Count(b => b.ProductId == a.Id))
+                .Take(4)
+                .ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("Product")]
+        public async Task<ActionResult<Product>> GetProduct(int? id)
+        {
+            if (id == null || _context == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+               .AsNoTracking()
+               .Include(x => x.ProductCategory)
+               .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return product;
+            }
+
+        }
     }
 }
