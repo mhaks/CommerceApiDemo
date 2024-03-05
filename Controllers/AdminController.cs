ead4d36406cbe4f07799b62a1acc397bfdc96e9b
mhaks <mike.hakanson@gmail.com>
2024-03-05 15:42:38 -0700

    added admin summary methods

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,152 @@
+﻿using CommerceApiDem.Data;
+using CommerceApiDem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommerceApiDemo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AdminController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AdminController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
+
+        }
+
+        [HttpGet]
+        [Route("SalesSummary")]
+        public async Task<ActionResult> SalesSummary(int Days)
+        {
+            if (_context == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var query = from o in _context.Order select o; ;
+
+            var lookbackDate = DateTime.Now.AddDays(-Days).Date;
+            query = query.Where(o => o.OrderHistory.Any(h => h.OrderStatusId == (int)OrderState.Processing && h.OrderDate >= lookbackDate));
+
+            var orders = await query
+                            .Include(o => o.OrderProducts)
+                            .Include(o => o.OrderHistory)
+                            .Include(c => c.User)
+                            .ThenInclude(c => c.StateLocation)
+                            .AsNoTracking()
+                            .ToListAsync();
+
+            var result = new
+            {
+                OrderCount = orders.Count,
+                ProductCount = orders.Sum(o => o.OrderProducts.Count),
+                Revenue = orders.Sum(o => o.OrderProducts.Sum(p => p.Price * p.Quantity))
+            };
+
+            return Ok(result);
+
+        }
+
+
+        [HttpGet]
+        [Route("OrderSummary")]
+        public async Task<ActionResult> OrderSummary(int? Days, int? Status)
+        {
+            if (_context == null || _context.Order == null)
+            {
+                return NotFound();
+            }
+
+            var query = from o in _context.Order select o;                            ;
+
+
+            if (Days.HasValue)
+            {
+                var lookbackDate = DateTime.Now.AddDays(-Days.Value).Date;
+                query = query.Where(o => o.OrderHistory.Any(h => h.OrderDate >= lookbackDate));
+            }
+
+            if (Status.HasValue)
+            {
+                query = query.Where(o => o.OrderHistory.OrderByDescending(h => h.OrderDate).First().OrderStatusId == Status.Value);
+            }
+
+            var orders = await query
+                            .Include(o => o.OrderProducts)
+                            .Include(o => o.OrderHistory)
+                            .Include(c => c.User)
+                            .ThenInclude(c => c.StateLocation)
+                            .AsNoTracking()
+                            .ToListAsync();
+
+
+
+            var result = new OrderSummary() { 
+                OrderCount = orders.Count, 
+                ProductCount = orders.Sum(o => o.OrderProducts.Count()),
+                Revenue = orders.Sum(o => o.Subtotal)
+            };
+            return Ok(result); 
+                                       
+
+        }
+
+        [HttpGet]
+        [Route("InventorySummary")]
+        public async Task<ActionResult> InventorySummary(int threshold)
+        {
+            if (_context == null || _context.Product == null)
+            {
+                return NotFound();
+            }
+
+            var query = from p in _context.Product select p;
+
+            var products = await query
+                            .Include(p => p.ProductCategory)                            
+                            .AsNoTracking()
+                            .ToListAsync();
+
+            var result = new
+            {
+                Active = products.Count(p => p.IsActive),
+                Inactive = products.Count(p => !p.IsActive),
+                AboveThreshold = products.Count(p => p.IsActive && p.AvailableQty > threshold),
+                BelowThreshold = products.Count(p => p.IsActive && p.AvailableQty <= threshold && p.AvailableQty > 0),
+                OutOfStock = products.Count(p => p.IsActive && p.AvailableQty == 0)
+            };
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("OrderStatus")]
+        public async Task<ActionResult<IEnumerable<OrderStatus>>> GetOrderStatuses()
+        {
+            if (_context == null || _context.OrderStatus == null)
+                return NotFound();
+
+            return await _context.OrderStatus
+                .AsNoTracking()                
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+        }
+    }
+
+    public class OrderSummary
+    {
+        public int OrderCount { get; set; }
+        public int ProductCount { get; set; }
+        public decimal Revenue { get; set; }       
+    }
+
+   
+}
